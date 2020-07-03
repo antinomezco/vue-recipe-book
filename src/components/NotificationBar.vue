fdeca30f9171415c0bb0125f4106d48602109bb7
Ubuntu <ubuntu@ip-172-31-54-240.ec2.internal>
2020-07-03 02:34:35 +0000

    added image and changed title

--- src/components/NotificationBar.vue
+++ src/components/NotificationBar.vue
@@ -14,7 +14,7 @@
 export default {
   name: 'NotificationBar',
   data: () => ({
-    notificationErrors: [{ color: 'red', id: 0, text: 'Things to do: 1) prettify 2) make it look like The Skullery' }]
+    notificationErrors: [{ color: 'red', id: 0, text: 'Things to do: 1) add data, 2) prettify 3) make it look like The Skullery' }]
   })
 }
 </script>
